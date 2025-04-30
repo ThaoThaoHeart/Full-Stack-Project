2911d97fcd03556c7bbe791221a0c46f8773f43d
Jenny <jennyhoang_9@hotmail.com>
2025-04-30 16:15:54 -0400

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "/choreo-apis/full-stack-project/backend/v1";
 
 const api = axios.create({
     // import anything in the .env file
